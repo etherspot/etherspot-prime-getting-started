325c6da006febb44098017b3b2e5de96bdb171a8
taylorferran <tailzpk@hotmail.co.uk>
2023-10-12 10:27:47 +0100

    Fix formatting

--- src/App.tsx
+++ src/App.tsx
@@ -17,30 +17,22 @@ const App = () => {
   const generateRandomEOA = async () => {
     // Create random EOA wallet
     const randomWallet = ethers.Wallet.createRandom();
-    // Set address and private key to pass into the Etherspot Prime SDK
     setEoaWalletAddress(randomWallet.address);
     setEoaPrivateKey(randomWallet.privateKey);
 }
 
   const generateEtherspotWallet = async () => {
-    // Initialise Etherspot SDK with the private key from the random wallet we created
-    // chainId is 80001 (mumbai) for this example
+    // Initialise Etherspot SDK
     const primeSdk = new PrimeSdk({ privateKey: eoaPrivateKey}, { chainId: 80001, projectKey: '' })
-
-    // Function to get the Etherspot wallet address
     const address: string = await primeSdk.getCounterFactualAddress();
     setEtherspotWalletAddress(address);
+    console.log('\x1b[33m%s\x1b[0m', `EtherspotWallet address: ${address}`);
   }
 
   return (
     <div className="App-header">
 
-      <head>
-        <link rel="shortcut icon" type="image/x-icon" href={logo} />
-      </head>
-
       <img className="App-logo" src={logo}></img>
-
       <h1  className="App-title">Getting started with Etherspot Prime</h1>
 
       <p> To initialise the SDK, it requires a Key Based Wallet(KBW) to be passed in.</p>
@@ -48,7 +40,7 @@ const App = () => {
       <button className="App-button" onClick={() => generateRandomEOA()}>
             First click here to generate a random KBW. 
       </button>
-      <a className="App-button" target="_blank" href={"https://mumbai.polygonscan.com/address/" + eoaWalletAddress}>
+      <a href={"https://mumbai.polygonscan.com/address/" + eoaWalletAddress}>
         KBW Address: {eoaWalletAddress}
       </a>
 
@@ -56,20 +48,19 @@ const App = () => {
         Now we can intialise the SDK with this address as the owner, and create an Etherspot smart contract wallet.
       </p>
 
-      <button className="App-button"  onClick={() => generateEtherspotWallet()}>
+      <button className="App-button" onClick={() => generateEtherspotWallet()}>
             Generate Etherspot Smart Contract Wallet
       </button>
-      <a className="App-button" target="_blank" href={"https://mumbai.polygonscan.com/address/" + etherspotWalletAddress}>
+      <a href={"https://mumbai.polygonscan.com/address/" + etherspotWalletAddress}>
     
         Etherspot Smart Account Address: {etherspotWalletAddress}
       </a>
 
       <p>
+           
         <a className="App-link" target="_blank" href="https://etherspot.fyi/prime-sdk/intro">
         Now you have a wallet created you can explore what else we can do with the Prime SDK.</a>
       </p>
-
-      <p>You can take a look at src/App.tsx to learn how we did this.</p>
     </div>
   )
 }
