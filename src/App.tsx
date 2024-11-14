3dfb653e8862dc571bd6862708ba08f08eaf4f07
ch4r10t33r <psramanuj@gmail.com>
2024-11-14 10:57:29 +0000

    CHanged from Mumbai to Amoy

--- src/App.tsx
+++ src/App.tsx
@@ -25,7 +25,7 @@ const App = () => {
 
     const bundlerApiKey = "eyJvcmciOiI2NTIzZjY5MzUwOTBmNzAwMDFiYjJkZWIiLCJpZCI6IjMxMDZiOGY2NTRhZTRhZTM4MGVjYjJiN2Q2NDMzMjM4IiwiaCI6Im11cm11cjEyOCJ9"
     // Initialise Etherspot SDK
-    const primeSdk = new PrimeSdk({ privateKey: eoaPrivateKey}, { chainId: 80001, bundlerProvider: new EtherspotBundler(80001, bundlerApiKey)  })
+    const primeSdk = new PrimeSdk({ privateKey: eoaPrivateKey}, { chainId: 80002, bundlerProvider: new EtherspotBundler(80002, bundlerApiKey)  })
     const address: string = await primeSdk.getCounterFactualAddress();
     setEtherspotWalletAddress(address);
     console.log('\x1b[33m%s\x1b[0m', `EtherspotWallet address: ${address}`);
@@ -42,7 +42,7 @@ const App = () => {
       <button className="App-button" onClick={() => generateRandomEOA()}>
             First click here to generate a random KBW. 
       </button>
-      <a href={"https://mumbai.polygonscan.com/address/" + eoaWalletAddress}>
+      <a href={"https://amoy.polygonscan.com/address/" + eoaWalletAddress}>
         KBW Address: {eoaWalletAddress}
       </a>
 
@@ -53,7 +53,7 @@ const App = () => {
       <button className="App-button" onClick={() => generateEtherspotWallet()}>
             Generate Etherspot Smart Contract Wallet
       </button>
-      <a href={"https://mumbai.polygonscan.com/address/" + etherspotWalletAddress}>
+      <a href={"https://amoy.polygonscan.com/address/" + etherspotWalletAddress}>
     
         Etherspot Smart Account Address: {etherspotWalletAddress}
       </a>
